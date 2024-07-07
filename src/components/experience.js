843c5da7cb0f92d230d345199db4c39c13f1aeed
salma el bakkouri <86151871+salma-elbakkouri@users.noreply.github.com>
2024-07-07 12:23:11 +0100

    changed projects & experience

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
-import { data } from 'autoprefixer';
+import taptap from '../assets/taptap-final.png'; // Import the image
+import alten from '../assets/alten.jpg'; // Import the image
+import aymd from '../assets/aymd.png'; // Import the image
+import mercure from '../assets/mercure.png'; // Import the image
 
 const experiences = [
     {
@@ -9,7 +10,8 @@ const experiences = [
         date: 'May 2024 - Present',
         title: 'Support IT Consultant',
         company: 'Alten Fes · Internship',
-        description: 'Actively engaged as a Supoort IT consultant at ALTEN DELIVERY CENTER MAROC, I handle IT support duties, addressing user issues and delivering effective solutions to ensure smooth operations',
+        description: 'Actively engaged as a Support IT consultant at ALTEN DELIVERY CENTER MAROC, I handle IT support duties, addressing user issues and delivering effective solutions to ensure smooth operations',
+        image: alten,
     },
     {
         id: 2,
@@ -17,6 +19,7 @@ const experiences = [
         title: 'Fullstack Developer',
         company: 'Aymd Agency · Internship',
         description: 'Currently developing a health assistant app utilizing artificial intelligence, built on React Native and Laravel, to streamline patient-doctor interactions and healthcare management',
+        image: aymd,
     },
     {
         id: 3,
@@ -24,28 +27,34 @@ const experiences = [
         title: 'Mobile Developer',
         company: 'Mercure I&S · Internship',
         description: 'I developed an Android app to locate on-duty pharmacies based on user location, utilizing Android, Java, XML, and Firebase',
+        image: mercure,
     },
     {
         id: 4,
         date: 'Jun 2021 - July 2021',
         title: 'Wordpress Developer',
         company: 'Mercure I&S · Internship',
-        description: 'I developed a web portal for an agricultural site using WordPress , elementor, CSS, and Bootstrap',
+        description: 'I developed a web portal for an agricultural site using WordPress, Elementor, CSS, and Bootstrap',
+        image: mercure,
     },
 ];
 
 const Experience = () => {
     return (
         <div style={{ minHeight: '100vh' }} id='experience' className='flex flex-col items-center justify-center relative'>
-            <h2 style={{ color: 'white', fontSize: '25px', fontWeight: '500', paddingTop: '80px' }}>My Experience</h2>
-            <div className="flex flex-col justify-center items-center gap-10 p-10 w-full max-w-3xl">
-                {experiences.map((exp) => (
-                    <div key={exp.id} className="bg-gray-500 bg-opacity-40 rounded-lg p-4 shadow-lg w-full cursor-pointer hover:shadow-gray hover:-translate-y-2 transition-shadow duration-300">
-                        {/* <h3 className="text-center text-white font-bold mb-4 text-2xl">{exp.date}</h3> */}
-                        <div className="flex items-center gap-4 justify-center">
-                            <div className='pl-3'>
-                                <h4 className="text-xl text-white font-bold pt-3">{exp.date} | {exp.company}</h4>
-                                <p className="text-gray-300 text-lg pt-2 pb-4">{exp.description}</p>
+            <h2 style={{ color: 'white', fontSize: '25px', fontWeight: '500', paddingTop: '120px' , marginBottom:'60px'}}>My Experience</h2>
+            <div className="flex flex-col justify-center items-center gap-40 p-10 w-full max-w-6xl">
+                {experiences.map((exp, index) => (
+                    <div key={exp.id} className={`flex ${index % 2 === 0 ? 'flex-row' : 'flex-row-reverse'} items-center w-full gap-16`}>
+                        <div className="relative flex flex-col items-start w-1/2">
+                            <div className="absolute top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/3 w-60 h-56 rounded-full z-0" style={{ boxShadow: '0 0 30px 30px rgba(67, 107, 168, 0.2), inset 0 0 30px 30px rgba(67, 107, 168, 0.2)' }} />
+                            <img src={exp.image} alt={exp.company} className="w-full h-auto rounded-sm z-10" />
+                        </div>
+                        <div className="flex flex-col items-start w-full">
+                            <h4 className="text-lg text-blue-400">{exp.date}</h4>
+                            <h4 className="text-lg text-white">{exp.company}</h4>
+                            <div className="bg-gray-500 bg-opacity-30 rounded-lg p-6 shadow-lg w-full mt-4 cursor-pointer hover:shadow-gray hover:-translate-y-2 transition-shadow duration-300">
+                                <p className="text-gray-300 text-lg">{exp.description}</p>
                             </div>
                         </div>
                     </div>
--- src/components/skills.js
+++ src/components/skills.js
@@ -1,56 +1,61 @@
-    import React from 'react';
-    import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-    import {
-        faReact, faAngular, faJsSquare, faNodeJs, faHtml5, faCss3Alt,
-        faBootstrap, faSass, faWordpress, faAndroid,
-        faNpm, faUbuntu
-    } from '@fortawesome/free-brands-svg-icons';
-    import { ReactComponent as FigmaSvg } from '../assets/figma.svg';
-    import { ReactComponent as JavaSvg } from '../assets/java.svg';
-    import { ReactComponent as PythonSvg } from '../assets/python.svg';
-    import { ReactComponent as PsSvg } from '../assets/ps.svg';
-    import { ReactComponent as TailwindSvg } from '../assets/tailwind.svg';
-    import { ReactComponent as CsharpSvg } from '../assets/csharp.svg';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faReact, faAngular, faJsSquare, faNodeJs, faHtml5, faCss3Alt,
+    faBootstrap, faSass, faWordpress, faAndroid,
+    faNpm
+    // faUbuntu
+} from '@fortawesome/free-brands-svg-icons';
+import { ReactComponent as FigmaSvg } from '../assets/figma.svg';
+import { ReactComponent as JavaSvg } from '../assets/java.svg';
+// import { ReactComponent as PythonSvg } from '../assets/python.svg';
+import { ReactComponent as PsSvg } from '../assets/ps.svg';
+import { ReactComponent as TailwindSvg } from '../assets/tailwind.svg';
+import { ReactComponent as CsharpSvg } from '../assets/csharp.svg';
 
-    const Skills = () => {
-        const skills = [
-            { id: 'react', name: 'React', icon: faReact, color: '#61DAFB' , },
-            { id: 'angular', name: 'Angular', icon: faAngular, color: '#DD0031' },
-            { id: 'javascript', name: 'JavaScript', icon: faJsSquare, color: '#F7DF1E' },
-            { id: 'nodejs', name: 'Node.js', icon: faNodeJs, color: '#339933' },
-            { id: 'npm', name: 'Npm', icon: faNpm, color: '#CB3837' },
-            { id: 'java', name: 'Java', svgIcon: JavaSvg },
-            { id: 'android', name: 'Android', icon: faAndroid, color: '#3DDC84' },
-            { id: 'csharp', name: 'C#', svgIcon : CsharpSvg },
-            { id: 'python', name: 'Python', svgIcon: PythonSvg }, 
-            { id: 'html', name: 'HTML', icon: faHtml5, color: '#E34F26' },
-            { id: 'css', name: 'CSS', icon: faCss3Alt, color: '#1572B6' },
-            { id: 'bootstrap', name: 'Bootstrap', icon: faBootstrap, color: '#7952B3' },
-            { id: 'sass', name: 'Sass', icon: faSass, color: '#CC6699' },
-            { id: 'tailwind', name: 'Tailwind', svgIcon : TailwindSvg },
-            { id: 'ubuntu', name: 'Ubuntu', icon: faUbuntu, color: '#d1822e' },
-            { id: 'figma', name: 'Figma', svgIcon: FigmaSvg },
-            { id: 'photoshop', name: 'Photoshop', svgIcon: PsSvg },
-            { id: 'wordpress', name: 'WordPress', icon: faWordpress, color: '#21759B' },
-        ];
+const Skills = () => {
+    const cardColor = '#282c34'; // Example card color
 
-        return (
-            <div style={{ minHeight: '100vh' }} id='skills' className='flex flex-col items-center justify-center'>
-                <h2 style={{ color: 'white', fontSize: '25px', fontWeight: '500', paddingTop: '80px' }}>My Skills</h2>
-                <div className="flex flex-row flex-wrap justify-center items-center gap-10 p-20">
-                    {skills.map(skill => (
-                        <div key={skill.id} className="flex flex-col items-center justify-center w-40 h-40 ">
-                            {skill.icon ? (
-                                <FontAwesomeIcon icon={skill.icon} size="3x" style={{ marginBottom: '10px', color: skill.color }} />
-                            ) : (
-                                <skill.svgIcon style={{ width: '50px', height: '50px', marginBottom: '10px' }} />
-                            )}
-                            <span className="text-white text-sm mt-2">{skill.name}</span>
-                        </div>
-                    ))}
-                </div>
+    const skills = [
+        { id: 'react', name: 'React', icon: faReact, color: '#61DAFB' },
+        { id: 'angular', name: 'Angular', icon: faAngular, color: '#DD0031' },
+        { id: 'javascript', name: 'JavaScript', icon: faJsSquare, color: '#F7DF1E' },
+        { id: 'nodejs', name: 'Node.js', icon: faNodeJs, color: '#339933' },
+        { id: 'npm', name: 'Npm', icon: faNpm, color: '#CB3837' },
+        { id: 'java', name: 'Java', svgIcon: JavaSvg },
+        { id: 'android', name: 'Android', icon: faAndroid, color: '#3DDC84' },
+        { id: 'csharp', name: 'C#', svgIcon: CsharpSvg },
+        // { id: 'python', name: 'Python', svgIcon: PythonSvg },
+        { id: 'html', name: 'HTML', icon: faHtml5, color: '#E34F26' },
+        { id: 'css', name: 'CSS', icon: faCss3Alt, color: '#1572B6' },
+        { id: 'bootstrap', name: 'Bootstrap', icon: faBootstrap, color: '#7952B3' },
+        { id: 'sass', name: 'Sass', icon: faSass, color: '#CC6699' },
+        { id: 'tailwind', name: 'Tailwind', svgIcon: TailwindSvg },
+        // { id: 'ubuntu', name: 'Ubuntu', icon: faUbuntu, color: '#d1822e' },
+        { id: 'figma', name: 'Figma', svgIcon: FigmaSvg },
+        { id: 'photoshop', name: 'Photoshop', svgIcon: PsSvg },
+        { id: 'wordpress', name: 'WordPress', icon: faWordpress, color: '#21759B' },
+    ];
+
+    return (
+        <div style={{ minHeight: '100vh', maxWidth: '800px', margin: '0 auto' }} id='skills' className='flex flex-col items-center justify-center'>
+            <h2 style={{ color: 'white', fontSize: '25px', fontWeight: '500', paddingTop: '150px', marginBottom: '30px', textAlign: 'center' }}>
+                - You can count on me if the project involves -
+            </h2>
+            
+            <div className="flex flex-row flex-wrap justify-center items-center gap-8 p-5">
+                {skills.map(skill => (
+                    <div key={skill.id} className="flex flex-col items-center justify-center w-20 h-20" style={{ backgroundColor: cardColor, borderRadius: '50%', padding: '5px', cursor: 'pointer' }}>
+                        {skill.icon ? (
+                            <FontAwesomeIcon icon={skill.icon} style={{ color: skill.color, fontSize: '2.2rem' }} />
+                        ) : (
+                            <skill.svgIcon style={{ width: '43px', height: '43px' }} />
+                        )}
+                    </div>
+                ))}
             </div>
-        );
-    };
+        </div>
+    );
+};
 
-    export default Skills;
+export default Skills;
